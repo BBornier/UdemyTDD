a175cd004ad5ef6d2778f538568596b32d5be0ba
Benoît B <benoitbornier@yahoo.fr>
2021-08-26 16:34:59 +0200

    chapitre 1, 2 et 3 des exercices TDD. Ecriture du premier test avec red, green, refactor.

---
+++ test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -0,0 +1,23 @@
+package com.virtualpairprogrammers.isbntools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValidateISBNTest {
+
+	@Test
+	public void checkAValidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449116);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void checkAnInvalidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449117);
+		assertFalse(result);
+	}
+
+}
